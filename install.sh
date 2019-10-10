ded8b1087b342bed0b1d10b2f4b7c2781d11e182
ctal80 <ctal80@gmail.com>
2019-10-10 15:43:52 +0300

    Adding pip proxy

--- install.sh
+++ install.sh
@@ -2,31 +2,31 @@ mkdir PTO
 wget https://raw.githubusercontent.com/FurqanKhan1/Dictator/master/nmapscan.sql
 cd PTO
 apt-get install python-mysqldb
-pip install pymetasploit
+pip install pymetasploit --proxy=http://proxy-us.intel.com:912
 wget https://raw.githubusercontent.com/FurqanKhan1/Dictator/master/nmapscan.sql
 git clone https://github.com/FurqanKhan1/Dictator.git
 git clone https://github.com/FurqanKhan1/Dictator_client.git
 apt-get install mdns-scan
-pip install python-nmap
-pip install python-libnmap
-pip install python-libnessus
-pip install lxml
-pip install psutil
-pip install django==1.11.10
-pip install djangorestframework==3.7.7
-pip install markdown       # Markdown support for the browsable API.
-pip install django-filter  # Filtering support
+pip install python-nmap --proxy=http://proxy-us.intel.com:912
+pip install python-libnmap --proxy=http://proxy-us.intel.com:912
+pip install python-libnessus --proxy=http://proxy-us.intel.com:912
+pip install lxml --proxy=http://proxy-us.intel.com:912
+pip install psutil --proxy=http://proxy-us.intel.com:912
+pip install django==1.11.10 --proxy=http://proxy-us.intel.com:912
+pip install djangorestframework==3.7.7 --proxy=http://proxy-us.intel.com:912
+pip install markdown  --proxy=http://proxy-us.intel.com:912     # Markdown support for the browsable API. 
+pip install django-filter --proxy=http://proxy-us.intel.com:912# Filtering support
 sudo apt-get install python-mysqldb
 apt-get install python-magic
-pip install texttable
-pip install pyshark==0.3.8
-pip install ansi2html
+pip install texttable --proxy=http://proxy-us.intel.com:912
+pip install pyshark==0.3.8 --proxy=http://proxy-us.intel.com:912
+pip install ansi2html --proxy=http://proxy-us.intel.com:912
 cd dictator_service/Dictator_service/Scripts/hoppy-1.8.1/hoppy-1.8.1-
 sudo make install
 cd ..
 git clone https://github.com/EnableSecurity/sipvicious.git
 cd sipvicious
-python setup.py install
+python setup.py install --proxy=http://proxy-us.intel.com:912
 cd ..
 service mysql stop
 apt-get --purge remove 'mysql*'
